ff1ded53b516239df80f7f03ac8c7f8bfd5bfd59
sum1 <sum01@users.noreply.github.com>
2017-03-31 16:06:09 -0400

    Change wxvcashgui to vcash-electron + more

--- vcash.sh
+++ vcash.sh
@@ -1,33 +1,26 @@
 # Maintainer: Sum1
 pkgbase=('vcash')
-pkgname=('vcash' 'wxvcashgui')
+pkgname=('vcash' 'vcash-electron')
 pkgver=0.6.0.2
 pkgrel=1
-pkgdesc="Vcash cryptocurrency installer (includes wallet and GUI)"
+pkgdesc="Vcash cryptocurrency installer (includes wallet and GUI). Use 'npm run ui' to start GUI."
 # If someone can confirm compability with other platforms then remember to update this & any dependancies
 arch=('x86_64')
 url="https://vcash.info/"
-license=('AGPL-3.0' 'unknown')
-depends=('openssl' 'db' 'boost' 'wxpython' 'qrencode')
-makedepends=('git' 'screen' 'curl' 'gcc')
-source=("https://github.com/openvcash/vcash/archive/${pkgver}.tar.gz" "https://github.com/openvcash/wxVcashGUI/archive/master.zip")
-sha256sums=('df604f14a3623c730604889ea806faadb6a7aff726b1654f70f5387dfe960d48' 'aea3fcb5f04a0b86068a9e4e0cf82fdccbadc3059b33eb9525289a072cbd28cc') #autofill using updpkgsums?
-
-build() {
-	mkdir -p ${srcdir}/${pkgbase}-${pkgver}/
-	cd "${srcdir}/${pkgbase}-${pkgver}/"
-
-	./configure --prefix=/usr
-	make
-}
+license=('AGPL-3.0' 'GPL-3.0')
+depends=('openssl' 'db' 'boost' 'nodejs')
+makedepends=('git' 'screen' 'curl' 'gcc<6.0')
+source=("https://github.com/openvcash/vcash/archive/${pkgver}.tar.gz" "https://github.com/whphhg/vcash-electron.git")
+sha256sums=('df604f14a3623c730604889ea806faadb6a7aff726b1654f70f5387dfe960d48' 'fd88c3efa7272f299a2911ac8217f5138b57036889f5124c5f1d5387bcaae218')
 
 package_vcash() {
-	backup=("~/.vcash/data" "~/.vcash/backup")
+	backup=("~/.vcash/backups/")
 	install=vcash.install
-	
-  	make DESTDIR="~/.vcash/" install
 }
 
-package_wxvcashgui() {
- 	make DESTDIR="~/.vcash/src/" install
+package_vcash-electron() {
+    mkdir -p ~/vcash/electron-gui/
+ 	git clone https://github.com/whphhg/vcash-electron.git ~/vcash/electron-gui/
+	cd ~/vcash/electron-gui/vcash-electron
+	npm install
 }
